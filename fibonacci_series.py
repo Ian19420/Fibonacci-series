1101df7e74f267d09fb1efe303a7dd9bebe787c3
Ian <glps1004@gmail.com>
2022-01-02 20:15:14 +0800

    add list

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -2,8 +2,12 @@ num = int(input("請輸入欲產生數目:"))
 
 def fab(max):
     n, a, b = 0, 0, 1
+    list_num = []
     while n < max:
-        print(b)
+        list_num.append(b)
         a, b = b, b + a
         n += 1
-fab(num)
+    return list_num
+
+for i in fab(num):
+    print(i)
