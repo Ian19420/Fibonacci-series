a21dff423e7ae589e7f5d7274e8db221acffc72b
Ian <glps1004@gmail.com>
2022-01-02 20:23:50 +0800

    add yield

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -2,12 +2,10 @@ num = int(input("請輸入欲產生數目:"))
 
 def fab(max):
     n, a, b = 0, 0, 1
-    list_num = []
     while n < max:
-        list_num.append(b)
+        yield b
         a, b = b, b + a
         n += 1
-    return list_num
 
 for i in fab(num):
     print(i)
